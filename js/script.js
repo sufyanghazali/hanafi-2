e92fa2dea649d2607561665ec4c045499870f537
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2020-07-18 22:21:01 +0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+const infoScreen = document.querySelector(".info__screen");
+const infoButton = document.querySelector(".info__button");
+
+const playButton = document.querySelector(".media__button--play");
+const technicolorAudio = document.querySelector("#technicolor__audio");
+
+const descButton = document.querySelector(".media__button--desc");
+const itemInfo = document.querySelector(".item__info");
+
+function changeClass(el, currentClass, newClass) {
+    el.classList.remove(currentClass);
+    el.classList.add(newClass);
+}
+
+function toggleVisibility(element, display) {
+    const elementStyles = window.getComputedStyle(element);
+    const currentDisplay = elementStyles.getPropertyValue("display");
+
+    if (currentDisplay === "none") {
+        element.style.display = display;
+    } else {
+        element.style.display = "none";
+    }
+}
+
+function clickInfo() {
+    toggleVisibility(infoScreen, "block");
+    console.log("wtf");
+}
+
+infoButton.addEventListener("click", clickInfo);
+
+
+
+playButton.addEventListener("click", () => {
+    console.log("play button")
+    if (technicolorAudio.paused) {
+        technicolorAudio.play();
+        changeClass(playButton, "fa-play", "fa-pause");
+    } else {
+        technicolorAudio.pause();
+        changeClass(playButton, "fa-pause", "fa-play");
+
+    }
+});
+
+descButton.addEventListener("click", () => {
+    toggleVisibility(itemInfo, "block");
+    descButton.classList.toggle("fa-plus");
+    descButton.classList.toggle("fa-minus");
+});
+
+
+/**
+ * 
+ * 1. check if the screen size is less than 768px
+ * 2. remove click event on info button
+ * 3. add onhover class
+ */
+
+function setInfo() {
+    console.log("fired");
+    if (window.innerWidth >= 700) {
+        infoButton.removeEventListener("click", clickInfo);
+        console.log("removed");
+        infoButton.classList.add("onhover");
+    }
+
+}
+window.addEventListener("load", setInfo);
+window.addEventListener("resize", setInfo);
